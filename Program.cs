c63ab0b3ff0aae9f482c07e4e2afc02427fc766a
VanHuy221 <23115053122221@sv.ute.udn.vn>
2025-02-27 09:07:29 +0700

    da code xong

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void ChaoHoi(string ten = "bạn")
+    {
+        Console.WriteLine($"Xin chào, {ten}!");
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhập tên của bạn (mặc định: bạn ): ");
+        string ten = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(ten))
+        {
+            ChaoHoi();
+        }
+        else
+        {
+            ChaoHoi(ten);
+        }
+    }
+}
